213141b8b868d09fced9a09a1083b89a35b8b7c6
Jenna Jaehnig <jjaehnig@umich.edu>
2024-01-10 19:54:30 -0500

    swapping almost works

--- drag_and_drop.js
+++ drag_and_drop.js
@@ -8,6 +8,7 @@ students.forEach(student => {
     })
 
     student.addEventListener('dragend', () => { // for opacity while dragging
+        console.log('end')
         student.classList.remove('dragging')
     })
 })
@@ -15,32 +16,53 @@ students.forEach(student => {
 tables.forEach(table => {
     table.addEventListener('dragover', e => {
         e.preventDefault()
+        // const desk = chooseDesk(table, e.clientY)
+        // console.log(desk)
         const desk = chooseDesk(table, e.clientX, e.clientY)
+        // console.log(desk)
         const currStudent = document.querySelector('.dragging')
         if(desk == null){
+            console.log('insert end')
+            // currStudent.parentNode.removeChild(currStudent)
             table.appendChild(currStudent)
         } else {
+            console.log('insert before')
+            // currStudent.parentNode.removeChild(currStudent)
+            const temp = currStudent.nextSibling
+            // console.log(temp.querySelector('img').src)
             table.insertBefore(currStudent, desk)
+            table.insertBefore(desk, temp)
         }
     })
 })
 
-function chooseDesk(table, x, y) {
-    const studentsAtGroup = [...table.querySelectorAll('.student:not(.dragging)')];
+function chooseDesk(table, x, y){
+    const desks = [...table.querySelectorAll('.student')]; 
 
-    return studentsAtGroup.reduce((closest, child) => {
-        const box = child.getBoundingClientRect();
-        const x_offset = x - box.left - box.width / 2;
-        const y_offset = y - box.top - box.height / 2;
-        const distance = Math.sqrt(x_offset ** 2 + y_offset ** 2);
-        console.log('distance: ' + distance)
-        console.log('closest distance: ' + closest.distance)
-        if (distance < closest.distance) {
-            return { distance, element: child, x_offset, y_offset };
-        } else {
-            console.log('closest: ')
-            console.log(closest)
-            return closest;
+    // if(x > desks[0].getBoundingClientRect().right && y > desks[0].getBoundingClientRect().bottom){
+    //     return null
+    // }
+
+    const distances = []
+    desks.forEach(desk => {
+        // console.log(desk.querySelector('img').src)
+        // console.log(desk.getBoundingClientRect())
+        const desk_center_x = desk.getBoundingClientRect().left + desk.getBoundingClientRect().width/2
+        const desk_center_y = desk.getBoundingClientRect().top + desk.getBoundingClientRect().height/2
+        const x_offset = x - desk_center_x
+        const y_offset = y - desk_center_y
+        const distance = Math.sqrt(x_offset ** 2 + y_offset ** 2)
+        distances.push(distance)
+    })
+    // console.log(distances)
+    var closest_desk_idx = 0
+    // console.log(distances.length)
+    for(var i = 1; i < distances.length; ++i){
+        if(distances[i] < distances[closest_desk_idx]){
+            closest_desk_idx = i
         }
-    }, { distance: Number.POSITIVE_INFINITY }).element;
+    }
+    console.log(table.children)
+    console.log(closest_desk_idx)
+    return table.children[closest_desk_idx]
 }
